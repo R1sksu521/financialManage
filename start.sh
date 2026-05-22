ae9138fb83f0871069b7df498c19228518d55fc4
r1sksu <15992490+r1sksu@user.noreply.gitee.com>
2026-05-22 15:16:50 +0800

    Switch to Procfile + Maven Tomcat plugin, no Docker

--- start.sh
+++ start.sh
@@ -7,8 +7,6 @@ DB_NAME=${MYSQLDATABASE:-financialmanage}
 DB_USER=${MYSQLUSER:-root}
 DB_PASS=${MYSQLPASSWORD:-root}
 
-# 写入 db.properties（src 目录下，Maven 编译时会复制到 target/classes）
-mkdir -p src/main/resources 2>/dev/null || true
 cat > src/db.properties << EOF
 jdbc.driver=com.mysql.jdbc.Driver
 jdbc.url=jdbc:mysql://${DB_HOST}:${DB_PORT}/${DB_NAME}?useUnicode=true&characterEncoding=utf-8&useSSL=false
@@ -17,5 +15,4 @@ jdbc.password=${DB_PASS}
 EOF
 
 echo "DB: ${DB_HOST}:${DB_PORT}/${DB_NAME}"
-
 exec mvn tomcat7:run
