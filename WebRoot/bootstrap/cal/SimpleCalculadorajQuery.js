0be46946954b18dc073150bc7587b7924d1da475
r1sksu <15992490+r1sksu@user.noreply.gitee.com>
2026-05-22 16:50:27 +0800

    Fix calculator: 0. bug + add backspace button

--- WebRoot/bootstrap/cal/SimpleCalculadorajQuery.js
+++ WebRoot/bootstrap/cal/SimpleCalculadorajQuery.js
@@ -1,11 +1,5 @@
 /*
- * jQuery SimpleCalculadora
- * @author dimti28@gmail.com - http://develoteca.com
- * @version 1.0
- * @date Julio 10, 2015
- * @category jQuery plugin
- * @copyright (c) 2015 dimti28@gmail.com (http://develoteca.com)
- * @license CC Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0) - http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * jQuery SimpleCalculadora - Fixed version
  */
  jQuery.fn.extend({Calculadora: function(op) {
 					var LaCalculadora=this;
@@ -14,7 +8,7 @@
 					var Clase;
     				var Botones;
 					var Signos;
-					
+
 					defaults = {
 						TituloHTML:'<a class="btn-block btn3d btn btn-success" href="javascript:void(0)">Calculator</a>',
 						Botones:["7","8","9","+","4","5","6","-","3","2","1","*","0",".","=","/"],
@@ -28,35 +22,46 @@
 						ClasetxtSalida:'form-control txtr',
 						InputBorrar:idInstancia+'Borrar',
 						ClaseInputBorrar:'btn3d btn btn-danger btn-lg btn-block',
-						EtiquetaBorrar:'Clear'
+						EtiquetaBorrar:'Clear',
+						InputRetroceso:idInstancia+'Retroceso',
+						EtiquetaRetroceso:'Del'
 					}
-					
+
                     var op = $.extend({}, defaults, op);
 					Botones=op.Botones;
 					Signos=op.Signos;
-                    $(LaCalculadora).append('<input type="text" class="'+op.ClasetxtSalida+'" id="'+op.txtSalida+'" value="0" >');
-					
-                    $(LaCalculadora).append('<div class="row" id="'+idInstancia+'btns"></div>');					$.each(Botones, function(index,value) {	
-						Clase=op.ClaseBtns1			
-						if(Signos.indexOf(value)>-1){Clase=op.ClaseBtns2;}
-						if(value=='='){Clase=op.ClaseBtns3;}
-						$('#'+idInstancia+'btns').append('<div class="'+op.ClaseColumnas+'"><input type="button" class="'+NombreBotonesClase+' '+op.ClaseBotones+Clase+'" value="'+value+'"/></div>');
-					});
-					
+                    $(LaCalculadora).append('<input type="text" class="'+op.ClasetxtSalida+'" id="'+op.txtSalida+'" value="0" readonly>');
+
+                    $(LaCalculadora).append('<div class="row" id="'+idInstancia+'btns"></div>');					$.each(Botones, function(index,value) {
+							Clase=op.ClaseBtns1
+							if(Signos.indexOf(value)>-1){Clase=op.ClaseBtns2;}
+							if(value=='='){Clase=op.ClaseBtns3;}
+							$('#'+idInstancia+'btns').append('<div class="'+op.ClaseColumnas+'"><input type="button" class="'+NombreBotonesClase+' '+op.ClaseBotones+Clase+'" value="'+value+'"/></div>');
+						});
+
                     $(LaCalculadora).append('<input type="button" id="'+op.InputBorrar+'" class="'+op.ClaseInputBorrar+'" value="'+op.EtiquetaBorrar+'">');
-					
+                    $(LaCalculadora).append('<input type="button" id="'+op.InputRetroceso+'" class="btn3d btn btn-warning btn-lg btn-block" value="'+op.EtiquetaRetroceso+'">');
+
                     $(LaCalculadora).html('<div class="panel panel-primary btn-block calculadoraBase  mtop">'+op.TituloHTML+'<div class="panel-body"><div class="col-md-12" style="margin-bottom: 10px;">'+$(LaCalculadora).html()+'</div></div> </div>');
-					
+
 					$('.'+NombreBotonesClase).click(function(){
 						var vTecla=$(this).val();
 						var salida=$('#'+op.txtSalida);
-						if(vTecla=='='){salida.val(eval(salida.val()));}
-						else{if((salida.val()==0)){
-							if(Signos.indexOf(vTecla)>-1){salida.val(0)}
+						if(vTecla=='='){
+							try { salida.val(eval(salida.val())); } catch(e) { salida.val('Error'); }
+						}
+						else if(salida.val()=='0' || salida.val()==''){
+							if(vTecla=='.'){salida.val('0.');}
+							else if(Signos.indexOf(vTecla)>-1){salida.val('0'+vTecla);}
 							else{salida.val(vTecla);}
-						}else{salida.val(salida.val()+vTecla);} 
+						}else{
+							salida.val(salida.val()+vTecla);
 						}
 					});
-					$("#"+op.InputBorrar).click(function(){$('#'+op.txtSalida).val("0");});		
+					$("#"+op.InputBorrar).click(function(){$('#'+op.txtSalida).val("0");});
+					$("#"+op.InputRetroceso).click(function(){
+						var s=$('#'+op.txtSalida).val();
+						$('#'+op.txtSalida).val(s.length>1 ? s.slice(0,-1) : '0');
+					});
 	}
 });
